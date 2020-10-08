e23e8dec0273a196d77d04301e2a29338c742841
JoaquinRozaMorales <ignacio.roza@alumnos.frm.utn.edu.ar>
2020-10-08 11:04:37 -0300

    getAll y getOne

--- src/main/java/com/example/alumno/controller/BaseController.java
+++ src/main/java/com/example/alumno/controller/BaseController.java
@@ -1,2 +1,18 @@
-package com.example.alumno.controller;public interface BaseController {
+package com.example.alumno.controller;
+
+import com.example.alumno.entities.Base;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.Serializable;
+
+public interface BaseController <E extends Base, ID extends Serializable> {
+
+    public ResponseEntity<?> getAll();
+    public ResponseEntity<?> getOne(@PathVariable ID id);
+
+
+
 }
+
