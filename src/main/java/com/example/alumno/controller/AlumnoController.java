b44eec35bc410a41303d995f17f198160fd9267d
JoaquinRozaMorales <ignacio.roza@alumnos.frm.utn.edu.ar>
2020-10-10 22:14:35 -0300

    corrección de clase alumnocontroller

--- src/main/java/com/example/alumno/controller/AlumnoController.java
+++ src/main/java/com/example/alumno/controller/AlumnoController.java
@@ -1,13 +1,2 @@
-package com.example.alumno.controller;
-
-import com.example.alumno.entities.Alumnos;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@CrossOrigin(origins = "*")
-@RequestMapping(path = "api/v1/alumnos")
-public class PersonaController extends BaseControllerImpl<Alumnos,AlumnosServiceImpl> {
-
+package com.example.alumno.controller;public class AlumnoController {
 }
